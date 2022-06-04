32e4df9adb50733c39834341f144f833197a87e2
afoliveira <afoliveira@ana.pt>
2022-06-04 23:06:59 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Manufy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
